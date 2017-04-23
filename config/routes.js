4fd7138b2aeb3d01e8d0ba177b94ddf7720ac98f
MarMinsk <mwheatley@hotmail.com>
2017-04-23 16:21:36 +0100

    fixed broken show controller

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ const router       = express.Router();
 // Controllers
 const books        = require('../controllers/books');
 
-//  router.get('/', (req, res) => res.render('statics/home'));
+router.get('/', (req, res) => res.render('statics/home'));
 
 router.get('/', (req, res) => res.render('index'));
 
