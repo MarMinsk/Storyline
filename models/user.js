5d438b4950f4809364c94386958f8afa00571034
MarMinsk <mwheatley@hotmail.com>
2017-04-24 10:08:44 +0100

    adding password validation

--- models/user.js
+++ models/user.js
@@ -1,10 +1,27 @@
 const mongoose      = require('mongoose');
-// const bcrypt     = require('bcrypt');
+const bcrypt        = require('bcrypt');
 
 const userSchema    = new mongoose.Schema({
   username: { type: String },
   email: { type: String, required: true },
   password: { type: String, required: true }
+});
+
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(this.isModified('password') && this._passwordConfimation !== this.password) this.invalidate('passwordConfirmation', 'does not match');
 });
 
 module.exports = mongoose.model('User', userSchema);
